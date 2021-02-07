5855830054a49b61f71d9994facaa8df253e71a2
akshayupadhyay239 <49569390+akshayupadhyay239@users.noreply.github.com>
2021-02-07 16:51:56 +0530

    changed props

--- client/src/Filecomp.js
+++ client/src/Filecomp.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Filecomp(props){
+    const link = "https://gateway.ipfs.io/ipfs/"+props.hash
     return <div className="file-comp">
         <p className="name">{props.id}. {props.filename}</p>
-        <button className="down-btn">download</button>
+        <button className="down-btn" target="_blank"><a href={link}>{props.hash}</a></button>
     </div>
 }
 
